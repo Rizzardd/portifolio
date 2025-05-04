22de553feac37610e218e74fbdb8bc0fec41090e
Monique de Jesus Silva <rizzarddirizu@gmail.com>
2025-05-04 01:29:23 -0400

    refactor: refatoração do código deixando mais limpo e menos repetitivo.

--- src/domains/home/components/FourthSection.tsx
+++ src/domains/home/components/FourthSection.tsx
@@ -1,11 +1,14 @@
 import { PiStarFourFill } from "react-icons/pi";
 
+
+
 import { IoLogoReact } from "react-icons/io5";
 import { RiNextjsFill, RiNodejsFill, RiTailwindCssFill } from "react-icons/ri";
 import { SiAxios, SiMongodb, SiTypescript } from "react-icons/si";
 import { FaGitAlt } from "react-icons/fa";
 import { IoLogoJavascript } from "react-icons/io";
 import Card from "@/shared/components/Card";
+import { stack } from "../constants/stack";
 
 export default function FourthSection() {
   return (
@@ -20,65 +23,21 @@ export default function FourthSection() {
         <h1 className="font-font-family font-semibold text-2xl">
           Growing skills and early achievements
         </h1>
-        <p className="pt-2 font-font-family">something</p>
+        <p className="pt-2 font-font-family">I've worked with different technologies over time. Here are some I'm experienced with:</p>
       </div>
- <div className="flex flex-col">
- <Card
-        icon={<IoLogoReact className="text-react w-8 h-8 mb-3 flex mx-auto" />}
-        title={"React.js"}
-        description="Expertise with using React.js, with hands-on experience building
-        scalable, component-based applications. I'm proficient in using hooks,
-        state management, optimizing performance, and integrating with
-        modern tooling and libraries. I write clean, maintainable code and
-        follow best practices to build efficient, user-focused interfaces."
-      />
-
-      <Card
-        icon={<RiNextjsFill className="text-white w-8 h-8" />}
-        title="Next.js"
-        description="TODO"
-      />
-
-<Card
-        icon={<RiTailwindCssFill className="text-tailwind w-8 h-8" />}
-        title="TailwindCSS"
-        description="TODO"
-      />
-
-<Card
-        icon={<SiAxios className="text-axios w-8 h-8" />}
-        title="Axios"
-        description="TODO"
-      />
-      <Card
-        icon={<SiTypescript className="text-typescript w-8 h-8 bg-white rounded" /> }
-        title="TypeScript"
-        description="TODO"
-      />
+ <div className="flex flex-wrap gap-2 justify-center items-center">
+ {stack.map((item, index) => (
           <Card
-        icon={<IoLogoJavascript className="text-black w-8 h-8 bg-javascript rounded" /> }
-        title="JavaScript"
-        description="TODO"
-      />
-
-<Card
-        icon={<RiNodejsFill className="text-node w-8 h-8 " /> }
-        title="Node.js"
-        description="TODO"
-      />
-      <Card
-        icon={<FaGitAlt className="text-git w-8 h-8 " /> }
-        title="Git"
-        description="TODO"
-      />
-
-<Card
-        icon={<SiMongodb className="text-mongodb w-8 h-8 " /> }
-        title="MongoDB"
-        description="TODO"
-      />
+            key={index}
+            image={item.image}
+            title={item.title}
+           
+          />
+        )
+ )}
       
  </div>
+ <p className="font-font-family font-normal text-sm mt-5"> ...and many more!</p>
     </div>
   );
 }
