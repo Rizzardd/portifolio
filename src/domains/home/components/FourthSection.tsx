f182f2b36b94de9205613c2e2c1f0da4ee6e9909
Monique de Jesus Silva <rizzarddirizu@gmail.com>
2025-05-04 01:33:29 -0400

    refactor: removendo código não utilizado.

--- src/domains/home/components/FourthSection.tsx
+++ src/domains/home/components/FourthSection.tsx
@@ -1,10 +1,5 @@
 import { PiStarFourFill } from "react-icons/pi";
 
-import { IoLogoReact } from "react-icons/io5";
-import { RiNextjsFill, RiNodejsFill, RiTailwindCssFill } from "react-icons/ri";
-import { SiAxios, SiMongodb, SiTypescript } from "react-icons/si";
-import { FaGitAlt } from "react-icons/fa";
-import { IoLogoJavascript } from "react-icons/io";
 import Card from "@/shared/components/Card";
 import { stack } from "../constants/stack";
 
