33b53ce14a230fbcbbb1dc7aa4ec764a14b11328
Monique de Jesus Silva <rizzarddirizu@gmail.com>
2025-05-04 01:35:35 -0400

    feat: adicionando novos dados a lista.

--- src/domains/home/constants/stack.ts
+++ src/domains/home/constants/stack.ts
@@ -1,1 +1,93 @@
-export const stack = [{ icon: "", title: "", description: "" }];
+export const stack = [
+  {
+    image: "/assets/images/logo-react.png",
+    title: "React",
+   
+  },
+  {
+    image: "/assets/images/logo-nextjs.png",
+    title: "Next.js",
+   
+  },
+  {
+    image: "/assets/images/logo-tanstack.png",
+    title: "Tanstack",
+   
+  },
+  {
+    image: "/assets/images/logo-tailwind.png",
+    title: "TailwindCSS",
+   
+  },
+  {
+    image: "/assets/images/logo-mongodb.png",
+    title: "MongoDB",
+   
+  },
+  {
+    image: "/assets/images/logo-chakraui.png",
+    title: "Chakra UI",
+   
+  },
+  {
+    image: "/assets/images/logo-zustand.png",
+    title: "Zustand",
+   
+  },
+  {
+    image: "/assets/images/logo-ts.png",
+    title: "Typescript",
+   
+  },
+  {
+    image: "/assets/images/logo-nodejs.png",
+    title: "Node.js",
+   
+  },
+  {
+    image: "/assets/images/logo-axios.png",
+    title: "Axios",
+   
+  },
+  {
+    image: "/assets/images/logo-git.png",
+    title: "Git",
+   
+  },
+  {
+    image: "/assets/images/logo-javascript.png",
+    title: "Javascript",
+   
+  },
+  {
+    image: "/assets/images/logo-vercel.png",
+    title: "Vercel",
+   
+  },
+  {
+    image: "/assets/images/logo-figma.png",
+    title: "Figma",
+   
+  },
+
+  {
+    image: "/assets/images/logo-express.png",
+    title: "Express.js",
+   
+  },
+  {
+    image: "/assets/images/logo-postman.png",
+    title: "Postman",
+   
+  },
+  {
+    image: "/assets/images/logo-npm.png",
+    title: "NPM",
+  },
+
+  {
+    image: "/assets/images/logo-github-white.png",
+    title: "GitHub",
+  }
+  
+];
